e24db07c61b90ddb685fab0eeca4d3f380f3f6c5
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2023-06-12 15:49:02 +0100

    test: remove azure tests for now

--- alzlib_test.go
+++ alzlib_test.go
@@ -9,8 +9,6 @@ import (
 	"os"
 	"testing"
 
-	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
-	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armpolicy"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -58,31 +56,31 @@ func Test_NewAlzLibDuplicateArchetypeDefinition(t *testing.T) {
 	assert.ErrorContains(t, err, "archetype with name duplicate already exists")
 }
 
-func TestGetBuiltInPolicy(t *testing.T) {
-	az := NewAlzLib()
-	cred, err := azidentity.NewDefaultAzureCredential(nil)
-	if err != nil {
-		t.SkipNow()
-	}
-	cf, _ := armpolicy.NewClientFactory("", cred, nil)
-	az.AddPolicyClient(cf)
-	err = az.GetBuiltInPolicies(context.Background(), []string{"8154e3b3-cc52-40be-9407-7756581d71f6"})
-	assert.NoError(t, err)
-	assert.Equal(t, 1, len(az.PolicyDefinitions))
-	assert.Equal(t, "Microsoft Managed Control 1614 - Developer Security Architecture And Design", *az.PolicyDefinitions["8154e3b3-cc52-40be-9407-7756581d71f6"].Properties.DisplayName)
-}
+// func TestGetBuiltInPolicy(t *testing.T) {
+// 	az := NewAlzLib()
+// 	cred, err := azidentity.NewDefaultAzureCredential(nil)
+// 	if err != nil {
+// 		t.SkipNow()
+// 	}
+// 	cf, _ := armpolicy.NewClientFactory("", cred, nil)
+// 	az.AddPolicyClient(cf)
+// 	err = az.GetBuiltInPolicies(context.Background(), []string{"8154e3b3-cc52-40be-9407-7756581d71f6"})
+// 	assert.NoError(t, err)
+// 	assert.Equal(t, 1, len(az.PolicyDefinitions))
+// 	assert.Equal(t, "Microsoft Managed Control 1614 - Developer Security Architecture And Design", *az.PolicyDefinitions["8154e3b3-cc52-40be-9407-7756581d71f6"].Properties.DisplayName)
+// }
 
-func TestGetBuiltInPolicySet(t *testing.T) {
-	az := NewAlzLib()
-	cred, err := azidentity.NewDefaultAzureCredential(nil)
-	if err != nil {
-		t.SkipNow()
-	}
-	cf, _ := armpolicy.NewClientFactory("", cred, nil)
-	az.AddPolicyClient(cf)
-	err = az.GetBuiltInPolicySets(context.Background(), []string{"7379ef4c-89b0-48b6-a5cc-fd3a75eaef93"})
-	assert.NoError(t, err)
-	assert.Equal(t, 1, len(az.PolicySetDefinitions))
-	assert.Equal(t, "Evaluate Private Link Usage Across All Supported Azure Resources", *az.PolicySetDefinitions["7379ef4c-89b0-48b6-a5cc-fd3a75eaef93"].Properties.DisplayName)
-	assert.Equal(t, 30, len(az.PolicyDefinitions))
-}
+// func TestGetBuiltInPolicySet(t *testing.T) {
+// 	az := NewAlzLib()
+// 	cred, err := azidentity.NewDefaultAzureCredential(nil)
+// 	if err != nil {
+// 		t.SkipNow()
+// 	}
+// 	cf, _ := armpolicy.NewClientFactory("", cred, nil)
+// 	az.AddPolicyClient(cf)
+// 	err = az.GetBuiltInPolicySets(context.Background(), []string{"7379ef4c-89b0-48b6-a5cc-fd3a75eaef93"})
+// 	assert.NoError(t, err)
+// 	assert.Equal(t, 1, len(az.PolicySetDefinitions))
+// 	assert.Equal(t, "Evaluate Private Link Usage Across All Supported Azure Resources", *az.PolicySetDefinitions["7379ef4c-89b0-48b6-a5cc-fd3a75eaef93"].Properties.DisplayName)
+// 	assert.Equal(t, 30, len(az.PolicyDefinitions))
+// }
